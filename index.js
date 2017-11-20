09908ceba7019d58186e5380bf5830b1991fb28c
Wescley Matos <wescleymatos@gmail.com>
2017-11-20 18:04:35 -0300

    teste de erro no heroku

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00-pnun5.mongodb.net:27017,cluster0-shard-00-01-pnun5.mongodb.net:27017,cluster0-shard-00-02-pnun5.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
+// const MongoClient = require('mongodb').MongoClient;
+// const ObjectID = require('mongodb').ObjectID;
+// const mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00-pnun5.mongodb.net:27017,cluster0-shard-00-01-pnun5.mongodb.net:27017,cluster0-shard-00-02-pnun5.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
 
 const path = require('path');
 const express = require('express');
@@ -21,19 +21,19 @@ app.set('views', path.join(__dirname, 'views'));
 //tipo de template
 app.set('view engine', 'ejs');
 
-app.get('/login', (req, res) => {
-  res.render('accounts/login');
-});
+// app.get('/login', (req, res) => {
+//   res.render('accounts/login');
+// });
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-MongoClient.connect(mongoUri, (err, db) => {
-  if (err) {
-    return;
-  }
-
-  app.db = db;
-  app.listen(app.get('port'), () => 'Server running...');
-});
+app.listen(app.get('port'), () => 'Server running...');
+// MongoClient.connect(mongoUri, (err, db) => {
+//   if (err) {
+//     return;
+//   }
+//
+//   app.db = db;
+// });
